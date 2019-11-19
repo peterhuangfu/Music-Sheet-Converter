0ac7a2ca433b8970f1c3f9c06b8e535c4e72bc70
jeter <b05705054@ntu.edu.tw>
2019-11-19 11:47:31 +0800

    lint

--- client/src/views/profile/index.vue
+++ client/src/views/profile/index.vue
@@ -2,21 +2,27 @@
   <div>
     <el-row>
       <el-col :span="9" style="background-color: white;">
-        <div style="color:#BFC4CC; float: right; padding: 20px">{{userId}}</div>
+        <div style="color:#BFC4CC; float: right; padding: 20px">
+          {{ userId }}
+        </div>
         <div
           style="display: flex; flex-direction: column; justify-content:space-around; align-items: center; margin-top: 100px; height: 500px"
         >
           <div
             style="width: 200px; height: 200px;border-radius: 50%; background-color: #6CC0AF;display: flex;justify-content:center; align-items: center;"
           >
-            <div style="color: white; font-weight: bold; font-size: 40px">U</div>
+            <div style="color: white; font-weight: bold; font-size: 40px">
+              U
+            </div>
           </div>
-          <div style="font-size: 20px">{{userName}}</div>
-          <div style="color: #A1ACBB">{{userGroup}}</div>
-          <div style="color: #69B8F0">{{userPosition}}</div>
+          <div style="font-size: 20px">{{ userName }}</div>
+          <div style="color: #A1ACBB">{{ userGroup }}</div>
+          <div style="color: #69B8F0">{{ userPosition }}</div>
           <div
             style="color: #8692A4; font-size: 10px; width: 100px; word-break: break-all"
-          >{{userDesc}}</div>
+          >
+            {{ userDesc }}
+          </div>
           <div
             style="width: 35px; height: 35px;border-radius: 50%; background-color: #F0F2F4;display: flex;justify-content:center; align-items: center;"
           >
@@ -30,7 +36,8 @@
           <el-col
             :span="3"
             style="color: #8A919F; font-size: 10px; height: 20px; border-right: 1px solid #222D3C; "
-          >{{sheetCount}} sheet(s) music</el-col>
+            >{{ sheetCount }} sheet(s) music</el-col
+          >
           <el-col
             :offset="1"
             :span="3"
@@ -69,18 +76,18 @@
 </template>
 <script>
 export default {
-  name: "Profile",
+  name: 'Profile',
   data() {
     return {
-      userId: "@user12345",
-      userName: "User1",
-      userGroup: "A Cappella",
-      userPosition: "Taipei",
-      userDesc: "Hi, i’m a sdjicnidscsdcindscndcnjdcnjcdcmkdcdkcndcdccd",
-      sheetCount: 0
+      userId: '@user12345',
+      userName: 'User1',
+      userGroup: 'A Cappella',
+      userPosition: 'Taipei',
+      userDesc: 'Hi, i’m a sdjicnidscsdcindscndcnjdcnjcdcmkdcdkcndcdccd',
+      sheetCount: 0,
     };
   },
-  methods: {}
+  methods: {},
 };
 </script>
 <style lang="stylus" scoped>
