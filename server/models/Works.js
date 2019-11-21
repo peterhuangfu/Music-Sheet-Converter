c9d3fdafd43ecf9c04debe785e28e2852f75795e
huang_fu <nightlight870213@gmail.com>
2019-11-21 20:23:01 +0800

    model change

--- server/models/Works.js
+++ server/models/Works.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const WorksSchema = Schema({
   file_id: { type: String, unique: true, required: true },
+  file_path: { type: String, unique: true, required: true },
   title: { type: String, required: true },
   description: String,
   uploader: { type: String, required: true },
