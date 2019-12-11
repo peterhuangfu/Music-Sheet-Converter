4d13203e1de2eeb8426f0c68b900fcd49748aafa
jeter <b05705054@ntu.edu.tw>
2019-12-11 21:31:51 +0800

    profile get

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,27 @@
+import agent from './agent';
+
 const Profile = {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    profiles: {},
+  },
+  mutations: {
+    GETPROFILE(state, payload) {
+      state.profiles = payload;
+    },
+  },
+  actions: {
+    getAllProfiles({ commit }) {
+      agent
+        .get(`profile/get_profile`)
+        .then(res => {
+          commit('GETPROFILE', res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+  },
   getters: {},
 };
 
--- client/src/views/profile/index.vue
+++ client/src/views/profile/index.vue
@@ -2,27 +2,21 @@
   <div>
     <el-row>
       <el-col :span="9" style="background-color: white;">
-        <div style="color:#BFC4CC; float: right; padding: 20px">
-          {{ userId }}
-        </div>
+        <div style="color:#BFC4CC; float: right; padding: 20px">{{ userId }}</div>
         <div
           style="display: flex; flex-direction: column; justify-content:space-around; align-items: center; margin-top: 100px; height: 500px"
         >
           <div
             style="width: 200px; height: 200px;border-radius: 50%; background-color: #6CC0AF;display: flex;justify-content:center; align-items: center;"
           >
-            <div style="color: white; font-weight: bold; font-size: 40px">
-              U
-            </div>
+            <div style="color: white; font-weight: bold; font-size: 40px">U</div>
           </div>
           <div style="font-size: 20px">{{ userName }}</div>
           <div style="color: #A1ACBB">{{ userGroup }}</div>
           <div style="color: #69B8F0">{{ userPosition }}</div>
           <div
             style="color: #8692A4; font-size: 10px; width: 100px; word-break: break-all"
-          >
-            {{ userDesc }}
-          </div>
+          >{{ userDesc }}</div>
           <div
             style="width: 35px; height: 35px;border-radius: 50%; background-color: #F0F2F4;display: flex;justify-content:center; align-items: center;"
           >
@@ -36,8 +30,7 @@
           <el-col
             :span="3"
             style="color: #8A919F; font-size: 10px; height: 20px; border-right: 1px solid #222D3C; "
-            >{{ sheetCount }} sheet(s) music</el-col
-          >
+          >{{ sheetCount }} sheet(s) music</el-col>
           <el-col
             :offset="1"
             :span="3"
@@ -75,6 +68,7 @@
   </div>
 </template>
 <script>
+import { mapState, mapActions } from 'vuex';
 export default {
   name: 'Profile',
   data() {
@@ -87,7 +81,39 @@ export default {
       sheetCount: 0,
     };
   },
-  methods: {},
+  computed: {
+    ...mapState({
+      isAuthenticated: state => state.auth.isAuthenticated,
+      isLoginCheck: state => state.auth.isLoginCheck,
+      switch_judge: state => state.auth.switch_judge,
+      profiles: state => state.profile.profiles,
+    }),
+  },
+  watch: {
+    profiles(newVal) {
+      // console.log(newVal);
+      this.userId = `@${newVal.google_id}`;
+      this.userName = newVal.username;
+      this.userDesc = newVal.self_tags;
+      this.userPosition = newVal.address;
+      this.userGroup = newVal.self_tags;
+      this.sheetCount = newVal.upload_works.length;
+    },
+    switch_judge: function(switch_judge) {
+      if (!this.isLoginCheck) {
+        this.$store.dispatch('auth/CheckLoginStatus');
+      } else {
+        if (this.isAuthenticated) this.getProfile();
+        else this.$router.push('/');
+      }
+    },
+  },
+  methods: {
+    getProfile() {
+      this.$store.dispatch('profile/getAllProfiles');
+      // console.log('start');
+    },
+  },
 };
 </script>
 <style lang="stylus" scoped>
