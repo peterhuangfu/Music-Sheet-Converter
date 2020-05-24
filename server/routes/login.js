c8e63be6ebe0ad791597bd047feb7debf1694141
peterhuangfu <b05705042@ntu.edu.tw>
2020-05-24 16:38:45 +0800

    edit google login get property

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,24 +6,28 @@ const User = require('../models/User')
 
 exports.GoogleLogin = async (req, res) => {
   const user = req.body.user
-  const current_user = await User.findOne({ google_id: user.El })
+  const user_keys = Object.keys(user)
+  const person_detail = user[user_keys[2]]
+  const person_detail_keys = Object.keys(user[user_keys[2]])
+  
+  const current_user = await User.findOne({ google_id: user[user_keys[0]] })
 
   if (!current_user) {
     const user_id = ObjectId()
     const newUser = new User({
       _id: user_id,
-      google_id: user.El,
-      username: user.w3.ig,
-      name: user.w3.wea+user.w3.ofa,
-      email: user.w3.U3,
+      google_id: user[user_keys[0]],
+      username: person_detail[person_detail_keys[1]],
+      name: person_detail[person_detail_keys[3]]+person_detail[person_detail_keys[2]],
+      email: person_detail[person_detail_keys[5]],
       self_description: '',
       self_tags: '',
       address: '',
-      profile_picture_url: user.w3.Paa,
+      profile_picture_url: person_detail[person_detail_keys[4]],
       upload_works: [],
       download_works: [],
-      google_token: user.Zi.access_token,
-      google_token_type: user.Zi.token_type,
+      google_token: user[user_keys[1]].access_token,
+      google_token_type: user[user_keys[1]].token_type,
       google_token_update_date: new Date(),
       sign_up_date: new Date()
     })
@@ -32,7 +36,7 @@ exports.GoogleLogin = async (req, res) => {
     req.session.current_user = newUser.toObject()
   }
   else {
-    const access_token = user.Zi.access_token
+    const access_token = user[user_keys[1]].access_token
     await User.updateOne({ _id: ObjectId(current_user._id) },{ $set:{ google_token: access_token, google_token_update_date: new Date() } })
     req.session.current_user = current_user.toObject()
   }
